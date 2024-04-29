398e493f60081a759a70277072079ab6404ac0d9
andocs <docs1502@gmail.com>
2024-04-29 17:52:23 +0800

    finished first draft

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={`${josefin.className} ${monea.variable}`}>{children}</body>
     </html>
   );
