58767ad1652ef860dd27712ae11742760f8ca76f
andocs <docs1502@gmail.com>
2024-04-29 15:27:01 +0800

    added header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div> asdsadsadasd </div>
+    <main>
+      <header className="mx-auto px-2 sm:px-6 lg:px-20 py-10">
+        <div className="relative flex h-16 flex-row justify-between">
+          <div className="flex">
+            <img src="https://s3-alpha-sig.figma.com/img/7da0/e629/c6e4ca599a048298830ce362ec5e3d5a?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NeAYXOOtWjCaak6eZ4LPNtLBuNxrLZ00gtYuogFSar3CpxbyRiwHmGgrmj-Y0quuU79o8U8erzdZvz00JmcYXXBSbeNznjlsjsGW0d37rMaRNN-rm~MCskQ5fbHyCsRromglHQbuNDpQx-Y9auusq6sSX4k0mpVuwooL7pM5s1jnYni6ujbEwcQ6Q0y31HgjHfrXzjrmM-X6~c8~l4Z0Umzvn6ok-y6XJB~oo0QA54XbRrEC653O0-xBR6NDUMeL0n1QiEPa5ES7l1OdEXZx3imeHJx3Wr90pSdKXgnVymPXFEmkxbGC3Pu3B9fOaPCLez9Po8Gqsff4kuS9wa3BYQ__" alt="" />
+          </div>
+          <div className="flex items-center">
+            <ul className="flex flex-row space-x-6 text-[#E0A701]">
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>PROJECT</li>
+              <li>SERVICE</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+        </div>
+
+      </header>
+
+    </main>
   )
 }
