6461d76c2d0e9843f8f8c642faac2a0179761899
savl <savlaguesma@gmail.com>
2024-04-30 15:44:59 +0800

    added photography content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -255,13 +255,45 @@ export default function Home() {
                   </div>
                 </div>
 
-                <div className="bg-[#E0A701] h-96 w-[32rem] text-[#383836]">
-                  PHOTOGRAPHY
+                {/* Card Section */}
+                <div className="flex flex-row justify-evenly">
+                  {/* Card 2 */}
+                  <div className="bg-[#E0A701] h-96 w-96 rounded-lg flex flex-col mt-6 p-6 justify-between">
+                    {/* Camera Icon */}
+                    <div className="flex justify-end">
+                      <svg
+                        className="w-10"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="rgba(53,53,53,1)"
+                      >
+                        <path d="M9.82843 5L7.82843 7H4V19H20V7H16.1716L14.1716 5H9.82843ZM9 3H15L17 5H21C21.5523 5 22 5.44772 22 6V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V6C2 5.44772 2.44772 5 3 5H7L9 3ZM12 18C8.96243 18 6.5 15.5376 6.5 12.5C6.5 9.46243 8.96243 7 12 7C15.0376 7 17.5 9.46243 17.5 12.5C17.5 15.5376 15.0376 18 12 18ZM12 16C13.933 16 15.5 14.433 15.5 12.5C15.5 10.567 13.933 9 12 9C10.067 9 8.5 10.567 8.5 12.5C8.5 14.433 10.067 16 12 16Z"></path>
+                      </svg>
+                    </div>
+
+                    {/* Text Group */}
+                    <div className="space-y-6">
+                      {/* Website Design Header */}
+                      <div>
+                        <h1 className="text-[#383836] text-4xl font-bold break-words">
+                          PHOTOGRAPHY
+                        </h1>
+                      </div>
+
+                      <p className="text-[#383836]">
+                        There are many elements in photography that come
+                        together to make an image be considered “good”. Some of
+                        these elements include, but are not limited to lighting,
+                        the rule of thirds, lines, shapes, texture, patterns,
+                        and color.
+                      </p>
+                    </div>
+                  </div>
                 </div>
 
                 {/* Cards Section */}
                 <div className="flex flex-row justify-evenly">
-                  {/* Card 2 */}
+                  {/* Card 3 */}
                   <div className="bg-[#383836] h-96 w-96 rounded-lg flex flex-col mt-6 p-6 justify-between">
                     {/* Computer Icon */}
                     <div className="flex justify-end">
