effc7bf32ae86080b7fb5fcc7bf1c80825cbc748
andocs <docs1502@gmail.com>
2024-04-30 14:45:57 +0800

    fixed formatting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -220,12 +220,18 @@ export default function Home() {
               </div>
               {/* Column 2 */}
               <div className="columns-3">
-                <div className="bg-[#383836]"><h1>WEB DESITE</h1>
-                <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-  <path d="M4 5V16H20V5H4ZM2 4.00748C2 3.45107 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44892 22 4.00748V18H2V4.00748ZM1 19H23V21H1V19Z"></path>
-</svg>
-</div>
-</div>
+                <div className="bg-[#383836]">
+                  <h1>WEB DESITE</h1>
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                    >
+                      <path d="M4 5V16H20V5H4ZM2 4.00748C2 3.45107 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44892 22 4.00748V18H2V4.00748ZM1 19H23V21H1V19Z"></path>
+                    </svg>
+                  </div>
+                </div>
                 <div className="bg-[#E0A701] text-[#383836]">PHOTOGRAPHY</div>
                 <div className="bg-[#383836]">MOBILE APP DESIGN</div>
               </div>
