c9fbcb3d6202593dafbd76162e4beb4fbb1c6695
andocs <docs1502@gmail.com>
2024-04-30 14:34:19 +0800

    finished about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -169,7 +169,7 @@ export default function Home() {
             <div className="w-2/3 space-y-8">
               <p className="text-2xl font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               <p className="text-2xl font-light">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
-              <a href="" className="p-0 items-center justify-start text-[#E0A701] text-xl w-fit flex flex-row hover:border-b border-[#E0A701]">
+              <a href="" className="p-0 items-center justify-start text-[#E0A701] text-xl w-fit flex flex-row relative after:bg-[#E0A701] after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300">
                 Read More{" "}
                 <svg
                   className="w-6"
