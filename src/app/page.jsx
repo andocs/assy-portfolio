a2ad16ff56906710b942603c362b1cfe28fccdd0
andoks <docs1502@gmail.com>
2024-06-16 18:06:50 +0800

    revisions not updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,10 +10,13 @@ export default function Home() {
       >
         <div className="relative flex h-20 pb-4 flex-row justify-between">
           <div className="flex">
+            <a href="#main">
             <img
-              src="https://s3-alpha-sig.figma.com/img/7da0/e629/c6e4ca599a048298830ce362ec5e3d5a?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NeAYXOOtWjCaak6eZ4LPNtLBuNxrLZ00gtYuogFSar3CpxbyRiwHmGgrmj-Y0quuU79o8U8erzdZvz00JmcYXXBSbeNznjlsjsGW0d37rMaRNN-rm~MCskQ5fbHyCsRromglHQbuNDpQx-Y9auusq6sSX4k0mpVuwooL7pM5s1jnYni6ujbEwcQ6Q0y31HgjHfrXzjrmM-X6~c8~l4Z0Umzvn6ok-y6XJB~oo0QA54XbRrEC653O0-xBR6NDUMeL0n1QiEPa5ES7l1OdEXZx3imeHJx3Wr90pSdKXgnVymPXFEmkxbGC3Pu3B9fOaPCLez9Po8Gqsff4kuS9wa3BYQ__"
+              src="/assets/assy-logo.png"
               alt=""
+              className="h-full"
             />
+            </a>
           </div>
           <div className="flex items-center">
             <ul className="flex flex-row space-x-6 text-[#E0A701]">
@@ -40,7 +43,7 @@ export default function Home() {
       {/* CONTENT */}
       <main className="relative mx-auto px-2 sm:px-6 lg:px-20 overflow-hidden">
         {/* MAIN SECTION */}
-        <section id="main" className="border-t border-[#E0A701] scroll-mt-32">
+        <section id="main" className="border-t border-[#E0A701]">
           {/* Wrapper */}
           <div className="mt-6">
             <div className="w-full min-h-screen flex flex-row justify-between">
